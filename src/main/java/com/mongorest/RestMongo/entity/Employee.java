24211ac146a09775fe85637fa52a4a65b13dca62
Shivku <sivakumar.sekar@rakuten.com>
2021-01-09 20:04:29 +0530

    Added mongodb integration.

--- src/main/java/com/mongorest/RestMongo/entity/Employee.java
+++ src/main/java/com/mongorest/RestMongo/entity/Employee.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
-@Document
+@Document(collection = "Employees")
 public class Employee {
     @Id
     private String id;
